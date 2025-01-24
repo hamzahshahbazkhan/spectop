ae33026443485a8dab3d1d4adbdccce10d7191f9
Hamzah Khan <khanhamzah142@gmail.com>
2025-01-24 22:28:31 +0530

    test

--- lib/fetchLaptopData.ts
+++ lib/fetchLaptopData.ts
@@ -67,30 +67,25 @@ const getProductDetails = async (url: string): Promise<Product | string> => {
   };
 
   let browser = null;
-  if (
-    process.env.NODE_ENV === "production" ||
-    process.env.VERCEL_ENV === "production"
-  ) {
-    const executablePath = await chromium.executablePath(
-      "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
-    );
-    browser = await puppeteerCore.launch({
-      executablePath,
-      args: [
-        ...chromium.args,
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--single-process", // Add this flag
-      ],
-      headless: chromium.headless,
-      defaultViewport: chromium.defaultViewport,
-    });
-  } else {
-    browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-  }
+  // if (
+  //   process.env.NODE_ENV === "production" ||
+  //   process.env.VERCEL_ENV === "production"
+  // ) {
+  const executablePath = await chromium.executablePath(
+    "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
+  );
+  browser = await puppeteerCore.launch({
+    executablePath,
+    args: chromium.args,
+    headless: chromium.headless,
+    defaultViewport: chromium.defaultViewport,
+  });
+  // } else {
+  //   browser = await puppeteer.launch({
+  //     headless: true,
+  //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  //   });
+  // }
   try {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2" });
